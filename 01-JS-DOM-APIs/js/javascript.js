7af8f5b215bbb9c624fc83023dd5299c7ee86076
Mauricio Navarro <navarromauricioj@gmail.com>
2020-03-08 20:08:31 -0300

    Topic-1 practice.3

--- 01-JS-DOM-APIs/js/javascript.js
+++ 01-JS-DOM-APIs/js/javascript.js
@@ -8,6 +8,39 @@ setTimeout(function() {
     },700);
 }
 
-function alertFunction(){  
-    alert("an alert message");
+
+function getJoke(){
+fetch("http://api.icndb.com/jokes/random")
+.then(function(response) { 
+return response.json();
+})
+.then(function(myJson) {            
+
+showTheJoke(myJson);
+
+})
+.catch(function(error){
+
+errorDetected();
+
+})
+}
+
+
+//the function to show the joke in the html
+
+function showTheJoke(myJson){
+document.getElementById("jokeHere").innerHTML= myJson.value.joke;
+}
+
+
+//the function changes the background to red in the section of the joke
+function errorDetected(){
+
+document.getElementById("jokeZone").style.backgroundColor="red";
+
+/* you can also use the function below if you also want to show the user that an error happened in the section of the joke
+document.getElementById("jokeHere").innerHTML= "An error has occurred";
+*/
+
 }

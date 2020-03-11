b7d28c01bb7aa6bb0a7c4806a77444b8073b62c1
Mauricio Navarro <navarromauricioj@gmail.com>
2020-03-11 05:23:14 -0300

    Topic-1 practice.6

--- 01-JS-DOM-APIs/js/javascript.js
+++ 01-JS-DOM-APIs/js/javascript.js
@@ -87,3 +87,107 @@ for(let rep of results){     /*iterate and show the elements in html*/
 }
 
 }
+
+
+function criptoTable() {
+const cryptoURL="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h%22%20-H%20%22accept:%20application/json";
+
+fetch(cryptoURL)
+.then(data => data.json())
+.then( data =>{
+
+    tableGenerator(data);
+    
+})
+.catch(data =>{
+
+    console.log("error detected");
+})
+
+
+}
+
+
+function tableGenerator(data){
+
+    const crypto=data;
+
+    const getTable = document.getElementById("table"); //select the section
+
+    const table =document.createElement("table"); //create the table
+
+    const headtable=document.createElement("tr"); //create the head of the table
+
+    const headName=document.createElement("th");    //assign the articles in the table header
+    const headSymbol=document.createElement("th");
+    const headCurrentPrice=document.createElement("th");
+    const headMarketCapRank=document.createElement("th");
+    const headAth=document.createElement("th");
+
+    headName.appendChild(document.createTextNode("name"));
+    headSymbol.appendChild(document.createTextNode("Symbol"));
+    headCurrentPrice.appendChild(document.createTextNode("Current Price"));
+    headMarketCapRank.appendChild(document.createTextNode("Market Carp Rank"));
+    headAth.appendChild(document.createTextNode("All Time High(ath)"));
+
+    headtable.appendChild(headName);
+    headtable.appendChild(headSymbol);
+    headtable.appendChild(headCurrentPrice);
+    headtable.appendChild(headMarketCapRank);
+    headtable.appendChild(headAth);
+
+
+    const tableBody = document.createElement("tbody") //create the body of the table
+
+    for(let index of crypto){    
+
+        const line = document.createElement("tr");  //create the table row 
+
+        const name = document.createElement("td");  //create the elements of the table row
+        
+        const symbol = document.createElement("td");
+
+        const price = document.createElement("td");
+
+        const markerCap = document.createElement("td");
+
+        const ath = document.createElement("td");
+
+        name.appendChild(document.createTextNode(index.name)); //assign the data 
+
+        symbol.appendChild(document.createTextNode(index.symbol));
+
+        price.appendChild(document.createTextNode("$ " + index.current_price.toFixed(4)));
+
+        markerCap.appendChild(document.createTextNode("#" + index.market_cap_rank));
+
+        ath.appendChild(document.createTextNode("$ " + index.ath.toFixed(4)));
+
+
+        line.appendChild(name);     
+
+        line.appendChild(symbol);
+
+        line.appendChild(price);
+
+        line.appendChild(markerCap);
+
+        line.appendChild(ath);
+        
+
+    
+        tableBody.appendChild(line);
+
+    }
+
+    table.appendChild(headtable);
+
+    table.appendChild(tableBody);
+
+    getTable.appendChild(table);
+
+
+}
+
+
+criptoTable(); //call the function to display the table 

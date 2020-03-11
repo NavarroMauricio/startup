aaed665e6f89e96bed6341e77ebbf2db5044c1d0
Mauricio Navarro <navarromauricioj@gmail.com>
2020-03-11 02:14:38 -0300

    Topic-1 practice.4
    Topic-1 practice.4 and some identation corrections

--- 01-JS-DOM-APIs/js/javascript.js
+++ 01-JS-DOM-APIs/js/javascript.js
@@ -10,15 +10,19 @@ setTimeout(function() {
 
 
 function getJoke(){
+
 fetch("http://api.icndb.com/jokes/random")
+
 .then(function(response) { 
 return response.json();
 })
+
 .then(function(myJson) {            
 
 showTheJoke(myJson);
 
 })
+
 .catch(function(error){
 
 errorDetected();
@@ -42,5 +46,44 @@ document.getElementById("jokeZone").style.backgroundColor="red";
 /* you can also use the function below if you also want to show the user that an error happened in the section of the joke
 document.getElementById("jokeHere").innerHTML= "An error has occurred";
 */
+
+}
+
+
+// point 4
+
+
+function search(){
+
+const parameter= document.getElementById("valueToSearch").value;
+
+const url=`https://api.github.com/search/repositories?q=${parameter}`;
+
+fetch(url)
+.then(data => data.json())
+.then(data =>{
+    showItem(data);
+})
+.catch(data =>{
+    console.log("error detected");
+})
+
+
+}
+
+function showItem(data) {
+
+const results = data.items;  /* pass only the necessary items*/ 
+
+const list = document.getElementById("reposList"); 
+
+for(let rep of results){     /*iterate and show the elements in html*/
+
+    let repName= document.createElement('li');
+
+    repName.innerHTML = rep.full_name;
+
+    list.appendChild(repName);    
+}
 
 }
